f93d718dda6322bcd419bf699e839cf1cd7bec37
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-25 13:05:25 -0300

    FEAT: adds DoubleNumber exercise

---
+++ exercises/DoubleNumber.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming_Logic_With_Csharp.exercises
+{
+    public class DoubleNumber
+    {
+        public int DoubleOfNumber(int number)
+        {
+            return number * 2;
+        }
+    }
+}
