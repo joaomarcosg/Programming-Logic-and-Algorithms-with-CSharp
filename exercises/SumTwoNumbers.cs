fd88e4f776d47a31cadcb443ac15dd250f22f201
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-26 16:55:59 -0300

    FEAT: adds SumTwoNumbers exercise

---
+++ exercises/SumTwoNumbers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming_Logic_With_Csharp.exercises
+{
+    public class SumTwoNumbers
+    {
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
