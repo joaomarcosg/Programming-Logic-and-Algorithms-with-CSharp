d34eec5f39425ec58aefab30d31104b34507ca04
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-26 16:51:52 -0300

    TEST: adds test with inputs

--- Programming-Logic-With-Csharp.Tests/DoubleNumberTest.cs
+++ Programming-Logic-With-Csharp.Tests/DoubleNumberTest.cs
@@ -18,5 +18,16 @@ namespace Programming_Logic_With_Csharp.Tests
 
             Assert.Equal(10, result);
         }
+
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(7, 14)]
+        [InlineData(-3, -6)]
+        public void DoubleOfNumberWithInputs(int input, int expected)
+        {
+            var doubleOfNumber = new DoubleNumber();
+            var result = doubleOfNumber.DoubleOfNumber(input);
+            Assert.Equal(expected, result);
+        }
     }
 }
