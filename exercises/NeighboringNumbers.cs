68502402be5e3806c04c7b6c7d69f200c529bff8
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-27 17:37:29 -0300

    FEAT: adds NeighboringNumbers exercise

---
+++ exercises/NeighboringNumbers.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programming_Logic_With_Csharp.exercises
+{
+    public class NeighboringNumbers
+    {
+        
+    }
+}
