86bc746cc7ad90e5987832656470b5532432fb84
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-29 16:26:16 -0300

    FEAT: finishes NeighboringNumbers exercise

--- exercises/NeighboringNumbers.cs
+++ exercises/NeighboringNumbers.cs
@@ -7,6 +7,9 @@ namespace Programming_Logic_With_Csharp.exercises
 {
     public class NeighboringNumbers
     {
-        
+        public (int prev, int next) CalcNeighbor(int num)
+        {
+            return (num - 1, num + 1);
+        }
     }
 }
