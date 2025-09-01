61ade874a018d2376a38a6dd34e6b2bcd7ae268c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-01 08:36:22 -0300

    TEST: adds PizzaAccount test

---
+++ Programming-Logic-With-Csharp.Tests/PizzaAccountTest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Programming_Logic_With_Csharp.exercises;
+using Xunit;
+
+namespace Programming_Logic_With_Csharp.Tests
+{
+    public class PizzaAccountTest
+    {
+        [Fact]
+
+        public void CalcPizzaAccountTest()
+        {
+            PizzaAccount calcPizzaAccount = new();
+
+            decimal result = calcPizzaAccount.CalcPizzaAccount(90, 3);
+
+            Assert.Equal(30, result);
+        }
+    }
+}
