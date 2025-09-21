9cb32db2f4b57a778e1e2116036d72d0ca73247c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-09-20 22:09:39 -0300

    TEST: finishes StorePaymentOptions test

--- Programming-Logic-With-Csharp.Tests/StorePaymentOptionsTest.cs
+++ Programming-Logic-With-Csharp.Tests/StorePaymentOptionsTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Programming_Logic_With_Csharp.exercises;
 using Xunit;
 
 namespace Programming_Logic_With_Csharp.Tests
@@ -11,7 +12,18 @@ namespace Programming_Logic_With_Csharp.Tests
         [Fact]
         public void PaymentOptionsPrintCorrectValues()
         {
-            
+            StorePaymentOptions payment = new StorePaymentOptions();
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            payment.PaymentOptions(60.00m);
+
+            var output = stringWriter.ToString();
+
+            Assert.Contains("Cash payment (10% off): 54", output);
+            Assert.Contains("54", output);
+            Assert.Contains("Payment in installments: 20", output);
+            Assert.Contains("20", output);
         }
     }
 }
